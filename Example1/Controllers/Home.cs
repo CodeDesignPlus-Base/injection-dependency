e2e39bcc4ca4612814f5e179b13ca34391fdb75b
Wilzon Liscano <wliscano@ventasyservicios.com.co>
2020-10-18 18:12:42 -0500

    Agregar archivos de proyecto.

---
+++ Example1/Controllers/Home.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Example1.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Example1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class Home : ControllerBase
+    {
+        private readonly MyDependency _dependency = new MyDependency();
+
+        public IActionResult Write()
+        {
+            var message = $"GUID: {Guid.NewGuid().ToString()}";
+
+            _dependency.WriteMessage(message);
+
+            return Ok();
+        }
+    }
+}
---
+++ Example1/Services/MyDependency.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Example1.Services
+{
+    public class MyDependency
+    {
+        public void WriteMessage(string message)
+        {
+            Console.WriteLine($"MyDependency.WriteMessage called. Message: {message}");
+        }
+    }
+}
